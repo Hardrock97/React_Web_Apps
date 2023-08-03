3258dbe13947d76ddc1c79c6bbc1ad9207828dfa
vicfirth@1997 <yashmundra1997@gmail.com>
2023-08-03 14:12:51 +0530

    JSX

--- App.js
+++ App.js
@@ -1,6 +1,39 @@
-const heading = React.createElement('h1',{
-  id:'heading'
-},"Starting with React")
+import React from "react"
+import  ReactDOM  from "react-dom/client"
+
+
+const JsxHeading = function() {
+
+return (
+<>
+<h1 id="heading" className="MainHeading">Hello there I have started learning React</h1>
+<h2>Make projects and learn more!</h2>
+<a href="https://www.google.com/">
+  <div>
+    <h1>Way to Google</h1>
+  </div>
+</a>
+</>
+)
+}
+  
+
+const para = (
+  <h3>Also, a Jsx element</h3>
+)
+
+
+const HeadingComponent = function() {
+  return (
+    <>
+    <h1>Heading Component</h1>
+    {para}
+    <JsxHeading/>
+    <JsxHeading></JsxHeading>
+    {JsxHeading()}
+    </>
+  )
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(heading)
+root.render(<HeadingComponent/>)
